5e956f24c57ef5df91577f14f2f6f4157336348d
jameermulani <jameer.mulani@promobitech.com>
2023-08-13 17:56:42 +0530

    Added scale animation to Splash screen when runs first time

--- app/src/main/java/com/example/borutoandroid/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/borutoandroid/presentation/screens/splash/SplashScreen.kt
@@ -1,19 +1,22 @@
 package com.example.borutoandroid.presentation.screens.splash
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import com.example.borutoandroid.R
@@ -22,11 +25,27 @@ import com.example.todoappstefan2023.ui.theme.Purple700
 
 @Composable
 fun SplashScreen(navHostController: NavHostController) {
-    Splash()
+
+    val scale = remember {
+        Animatable(initialValue = 0f)
+    }
+
+    //LaunchedEffect runs only the first time of composable execuation.
+    LaunchedEffect(key1 = true){
+        scale.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(
+                durationMillis = 500,
+                delayMillis = 250,
+            )
+        )
+    }
+
+    Splash(degrees = scale.value)
 }
 
 @Composable
-fun Splash() {
+fun Splash(degrees : Float) {
 
     val modifier: Modifier = if (isSystemInDarkTheme()) {
         Modifier
@@ -45,7 +64,7 @@ fun Splash() {
             .fillMaxSize()
     }
 
-    Box(modifier = modifier, contentAlignment = Alignment.Center) {
+    Box(modifier = modifier.scale(degrees), contentAlignment = Alignment.Center) {
         Image(
             painter = painterResource(id = R.drawable.ic_logo),
             contentDescription = stringResource(R.string.app_logo)
@@ -57,5 +76,5 @@ fun Splash() {
 @Preview
 @Composable
 fun SplashPreview() {
-    Splash()
+    Splash(0f)
 }
